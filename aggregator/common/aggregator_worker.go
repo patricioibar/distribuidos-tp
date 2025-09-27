2b383ba373fc4579126654fc595cf9a91e576670
Patricio Ibar <pibar@fi.uba.ar>
2025-09-27 02:08:28 -0300

    improving names and extracting common functs to utils

--- aggregator/common/aggregator_worker.go
+++ aggregator/common/aggregator_worker.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	a "aggregator/common/aggFunctions"
-	"fmt"
 
 	"github.com/op/go-logging"
 	ic "github.com/patricioibar/distribuidos-tp/innercommunication"
@@ -17,27 +16,27 @@ type AggregatorWorker struct {
 	Config    *Config
 	input     mw.MessageMiddleware
 	output    mw.MessageMiddleware
-	onMessage mw.OnMessageCallback
+	callback  mw.OnMessageCallback
 	batchChan chan ic.RowsBatch
 	closeChan chan struct{}
 }
 
 func NewAggregatorWorker(config *Config, input mw.MessageMiddleware, output mw.MessageMiddleware) *AggregatorWorker {
 	batchChan := make(chan ic.RowsBatch, maxBatchBufferSize)
-	onMessage := onMessageFromConfig(config, batchChan)
+	onMessage := aggregatorMessageCallback(config, batchChan)
 
 	return &AggregatorWorker{
 		Config:    config,
 		input:     input,
 		output:    output,
-		onMessage: onMessage,
+		callback:  onMessage,
 		batchChan: batchChan,
 		closeChan: make(chan struct{}),
 	}
 }
 
 func (a *AggregatorWorker) Start() {
-	if err := a.input.StartConsuming(a.onMessage); err != nil {
+	if err := a.input.StartConsuming(a.callback); err != nil {
 		a.Close()
 		log.Fatalf("Failed to start consuming messages: %v", err)
 	}
@@ -69,7 +68,7 @@ func (a *AggregatorWorker) Close() {
 	close(a.closeChan)
 }
 
-func onMessageFromConfig(config *Config, batchChan chan ic.RowsBatch) mw.OnMessageCallback {
+func aggregatorMessageCallback(config *Config, batchChan chan ic.RowsBatch) mw.OnMessageCallback {
 	return func(consumeChannel mw.MiddlewareMessage, done chan *mw.MessageMiddlewareError) {
 
 		jsonStr := string(consumeChannel.Body)
@@ -136,79 +135,4 @@ func aggregateRows(batch *ic.RowsBatch, config *Config) ([][]interface{}, error)
 	result := getAggregatedRowsFromGroupedData(groupedData)
 
 	return result, nil
-}
-
-func getAggregatedRowsFromGroupedData(groupedData map[string][]a.Aggregation) [][]interface{} {
-	var result [][]interface{}
-	for key, aggs := range groupedData {
-		row := []interface{}{key}
-		for _, agg := range aggs {
-			row = append(row, agg.Result())
-		}
-		result = append(result, row)
-	}
-	return result
-}
-
-func getAggColIndexes(config *Config, batch *ic.RowsBatch) map[string]int {
-	var aggColIndexes map[string]int = make(map[string]int)
-	for _, agg := range config.Aggregations {
-		for i, colName := range batch.ColumnNames {
-			if colName == agg.Col {
-				aggColIndexes[agg.Col] = i
-				break
-			}
-		}
-	}
-	return aggColIndexes
-}
-
-func getGroupByColIndexes(config *Config, batch *ic.RowsBatch) []int {
-	var groupByIndexes []int
-	for _, groupByCol := range config.GroupBy {
-		for i, colName := range batch.ColumnNames {
-			if colName == groupByCol {
-				groupByIndexes = append(groupByIndexes, i)
-				break
-			}
-		}
-	}
-	return groupByIndexes
-}
-
-func getGroupByKey(groupByIndexes []int, row []interface{}) string {
-	var keyParts []string
-	for _, idx := range groupByIndexes {
-		stringKey := fmt.Sprintf("%v", row[idx])
-		keyParts = append(keyParts, stringKey)
-	}
-	return joinParts(keyParts, "-")
-}
-
-func joinParts(keyParts []string, separator string) string {
-	key := ""
-	if len(keyParts) > 0 {
-		key = keyParts[0]
-		for i := 1; i < len(keyParts); i++ {
-			key += separator + keyParts[i]
-		}
-	}
-	return key
-}
-
-func getBatchFromAggregatedRows(config *Config, aggregatedRows [][]interface{}) ic.RowsBatch {
-	var aggregatedColumnNames []string
-
-	groupedColName := joinParts(config.GroupBy, "-")
-	aggregatedColumnNames = append(aggregatedColumnNames, groupedColName)
-
-	for _, agg := range config.Aggregations {
-		aggColName := joinParts([]string{agg.Func, agg.Col}, "_")
-		aggregatedColumnNames = append(aggregatedColumnNames, aggColName)
-	}
-
-	return ic.RowsBatch{
-		ColumnNames: aggregatedColumnNames,
-		Rows:        aggregatedRows,
-	}
 }
---
+++ aggregator/common/utils.go
@@ -0,0 +1,83 @@
+package common
+
+import (
+	a "aggregator/common/aggFunctions"
+	"fmt"
+
+	ic "github.com/patricioibar/distribuidos-tp/innercommunication"
+)
+
+func getAggregatedRowsFromGroupedData(groupedData map[string][]a.Aggregation) [][]interface{} {
+	var result [][]interface{}
+	for key, aggs := range groupedData {
+		row := []interface{}{key}
+		for _, agg := range aggs {
+			row = append(row, agg.Result())
+		}
+		result = append(result, row)
+	}
+	return result
+}
+
+func getAggColIndexes(config *Config, batch *ic.RowsBatch) map[string]int {
+	var aggColIndexes map[string]int = make(map[string]int)
+	for _, agg := range config.Aggregations {
+		for i, colName := range batch.ColumnNames {
+			if colName == agg.Col {
+				aggColIndexes[agg.Col] = i
+				break
+			}
+		}
+	}
+	return aggColIndexes
+}
+
+func getGroupByColIndexes(config *Config, batch *ic.RowsBatch) []int {
+	var groupByIndexes []int
+	for _, groupByCol := range config.GroupBy {
+		for i, colName := range batch.ColumnNames {
+			if colName == groupByCol {
+				groupByIndexes = append(groupByIndexes, i)
+				break
+			}
+		}
+	}
+	return groupByIndexes
+}
+
+func getGroupByKey(groupByIndexes []int, row []interface{}) string {
+	var keyParts []string
+	for _, idx := range groupByIndexes {
+		stringKey := fmt.Sprintf("%v", row[idx])
+		keyParts = append(keyParts, stringKey)
+	}
+	return joinParts(keyParts, "-")
+}
+
+func joinParts(keyParts []string, separator string) string {
+	key := ""
+	if len(keyParts) > 0 {
+		key = keyParts[0]
+		for i := 1; i < len(keyParts); i++ {
+			key += separator + keyParts[i]
+		}
+	}
+	return key
+}
+
+func getBatchFromAggregatedRows(config *Config, aggregatedRows [][]interface{}) ic.RowsBatch {
+	var aggregatedColumnNames []string
+
+	groupedColName := joinParts(config.GroupBy, "-")
+	aggregatedColumnNames = append(aggregatedColumnNames, groupedColName)
+
+	for _, agg := range config.Aggregations {
+		aggColName := joinParts([]string{agg.Func, agg.Col}, "_")
+		aggregatedColumnNames = append(aggregatedColumnNames, aggColName)
+	}
+
+	return ic.RowsBatch{
+		ColumnNames: aggregatedColumnNames,
+		Rows:        aggregatedRows,
+	}
+}
