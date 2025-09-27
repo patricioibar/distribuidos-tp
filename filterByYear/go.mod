b4708c2cf2d928a5aadd7db201c5706fde822734
Valentín Marturet <valen.martu2206@gmail.com>
2025-09-26 22:53:45 -0300

    Modificando toda la logica del filter para utilizar la interfaz

--- filterByYear/go.mod
+++ filterByYear/go.mod
@@ -3,3 +3,13 @@ module filter
 go 1.25.1
 
 require github.com/rabbitmq/amqp091-go v1.10.0
+
+require (
+	github.com/patricioibar/distribuidos-tp/innercommunication v0.0.0-00010101000000-000000000000
+	github.com/patricioibar/distribuidos-tp/middleware v0.0.0-00010101000000-000000000000
+	github.com/spf13/viper v1.21.0
+)
+
+replace github.com/patricioibar/distribuidos-tp/innercommunication => ../innercommunication
+
+replace github.com/patricioibar/distribuidos-tp/middleware => ../middleware
---
+++ filterByYear/main.go
@@ -0,0 +1,37 @@
+package filterbyyear
+
+import (
+	"log"
+
+	mw "github.com/patricioibar/distribuidos-tp/middleware"
+)
+
+
+
+
+func main() {
+
+
+	var input mw.MessageMiddleware
+	var output mw.MessageMiddleware
+
+
+
+	consumerName := "transactionToFilterByYear"
+
+	input, err := mw.NewConsumer(consumerName, "transactions", "amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to create input consumer: %v", err)
+	}
+	output, err = mw.NewProducer("filteredTransactionsByYear", "amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to create output producer: %v", err)
+	}
+
+	filter := NewFilterByYear(input, output)
+	filter.Start()
+}
+
+
+
+
