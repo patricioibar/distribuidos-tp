7cc40c6f9ad014ca66b31ca187fa2a4ee84aae40
Patricio Ibar <pibar@fi.uba.ar>
2025-11-28 18:28:30 -0300

    testing filter duplicated end signals

--- filter/common/filter_test.go
+++ filter/common/filter_test.go
@@ -2,6 +2,7 @@ package filter
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
@@ -9,83 +10,120 @@ import (
 	mw "github.com/patricioibar/distribuidos-tp/middleware"
 )
 
-// Mock implementations for testing
-type MockMiddleware struct {
-	messages          chan []byte
-	onMessage         mw.OnMessageCallback
-	isConsuming       bool
-	shouldFailSend    bool
-	shouldFailConsume bool
-	deletedChan       chan struct{}
+type StubProducer struct {
+	sentMessages [][]byte
+	newMessage   chan struct{}
 }
 
-func NewMockMiddleware() *MockMiddleware {
-	return &MockMiddleware{
-		messages:    make(chan []byte, 100),
+func newStubProducer() *StubProducer {
+	return &StubProducer{sentMessages: make([][]byte, 0), newMessage: make(chan struct{}, 10)}
+}
+
+func (s *StubProducer) waitForAMessage() {
+	<-s.newMessage
+}
+
+func (s *StubProducer) Send(message []byte) (error *mw.MessageMiddlewareError) {
+	s.sentMessages = append(s.sentMessages, message)
+	s.newMessage <- struct{}{}
+	return nil
+}
+
+func (s *StubProducer) StartConsuming(onMessageCallback mw.OnMessageCallback) (error *mw.MessageMiddlewareError) {
+	return nil
+}
+
+func (s *StubProducer) StopConsuming() (error *mw.MessageMiddlewareError) { return nil }
+
+func (s *StubProducer) Close() (error *mw.MessageMiddlewareError) { return nil }
+
+func (s *StubProducer) Delete() (error *mw.MessageMiddlewareError) { return nil }
+
+type StubConsumer struct {
+	onMessages  []mw.OnMessageCallback
+	lastCalled  int
+	started     chan struct{}
+	deletedChan chan struct{}
+	deleted     bool
+}
+
+func newStubConsumer() *StubConsumer {
+	return &StubConsumer{
+		started:     make(chan struct{}, 10),
 		deletedChan: make(chan struct{}),
+		onMessages:  make([]mw.OnMessageCallback, 0),
+		lastCalled:  0,
+		deleted:     false,
 	}
 }
 
-func (m *MockMiddleware) StartConsuming(callback mw.OnMessageCallback) *mw.MessageMiddlewareError {
-	if m.shouldFailConsume {
-		return &mw.MessageMiddlewareError{
-			Code: mw.MessageMiddlewareMessageError,
-			Msg:  "mock consume error",
+func (s *StubConsumer) waitForStart() {
+	<-s.started
+}
+
+func (s *StubConsumer) StartConsuming(onMessageCallback mw.OnMessageCallback) (error *mw.MessageMiddlewareError) {
+	println("StubConsumer started")
+	s.onMessages = append(s.onMessages, onMessageCallback)
+	s.started <- struct{}{}
+	<-s.deletedChan
+	return nil
+}
+
+func (s *StubConsumer) InjectMessage(message []byte, doneChan chan *mw.MessageMiddlewareError) {
+	if len(s.onMessages) == 0 {
+		return
+	}
+	// Pick a random callback using math/rand
+	// calling := rand.Intn(len(s.onMessages))
+	calling := s.lastCalled
+	s.lastCalled = (s.lastCalled + 1) % len(s.onMessages)
+	s.onMessages[calling](mw.MiddlewareMessage{Body: message}, doneChan)
+}
+
+func (s *StubConsumer) SimulateMessage(message []byte) {
+	if s.deleted {
+		return
+	}
+	println("Simulating message: ", string(message))
+	doneChan := make(chan *mw.MessageMiddlewareError)
+	msgCopy := make([]byte, len(message))
+	copy(msgCopy, message)
+	go func() {
+		if err := <-doneChan; err != nil {
+			print("#")
+			// If the consumer was deleted in the meantime, don't requeue.
+			select {
+			case <-s.deletedChan:
+				// input deleted, drop the message
+				return
+			default:
+				s.SimulateMessage(msgCopy)
+			}
 		}
-	}
+	}()
+	// Inject a copy so the handler doesn't share the same underlying buffer when requeued
+	s.InjectMessage(msgCopy, doneChan)
+}
 
-	m.onMessage = callback
-	m.isConsuming = true
-	<-m.deletedChan
+func (s *StubConsumer) Send(message []byte) (error *mw.MessageMiddlewareError) {
+	s.SimulateMessage(message)
 	return nil
 }
 
-func (m *MockMiddleware) StopConsuming() *mw.MessageMiddlewareError {
-	m.isConsuming = false
+func (s *StubConsumer) StopConsuming() (error *mw.MessageMiddlewareError) { return nil }
+
+func (s *StubConsumer) Close() (error *mw.MessageMiddlewareError) { return nil }
+
+func (s *StubConsumer) Delete() (error *mw.MessageMiddlewareError) {
+	close(s.deletedChan)
+	s.deleted = true
 	return nil
-}
-
-func (m *MockMiddleware) Send(message []byte) *mw.MessageMiddlewareError {
-
-	if m.shouldFailSend {
-		return &mw.MessageMiddlewareError{
-			Code: mw.MessageMiddlewareMessageError,
-			Msg:  "mock send error",
-		}
-	}
-
-	m.messages <- message
-	return nil
-}
-
-func (m *MockMiddleware) Close() *mw.MessageMiddlewareError {
-	return nil
-}
-
-func (m *MockMiddleware) Delete() *mw.MessageMiddlewareError {
-	close(m.deletedChan)
-	return nil
-}
-
-func (m *MockMiddleware) GetOneMessage() []byte {
-	return <-m.messages
-}
-
-func (m *MockMiddleware) SimulateMessage(data []byte) {
-	if m.onMessage != nil && m.isConsuming {
-		message := mw.MiddlewareMessage{
-			Body: data,
-		}
-		done := make(chan *mw.MessageMiddlewareError, 1)
-		m.onMessage(message, done)
-		<-done // wait for processing to complete
-	}
 }
 
 // Test for NewFilter with updated signature (workerID parameter)
 func TestNewFilter(t *testing.T) {
-	input := NewMockMiddleware()
-	output := NewMockMiddleware()
+	input := newStubConsumer()
+	output := newStubProducer()
 
 	filter := NewFilter("test-worker-1", input, output, "TbyYear", 1, nil, "job-123")
 
@@ -154,8 +192,8 @@ func TestGetFilterFunction(t *testing.T) {
 
 // Test callback function behavior with end signals
 func TestFilterCallbackWithEndSignal(t *testing.T) {
-	input := NewMockMiddleware()
-	output := NewMockMiddleware()
+	input := newStubConsumer()
+	output := newStubProducer()
 
 	// Create filter worker
 	filterWorker := NewFilter("test-worker", input, output, "TbyYear", 1, nil, "job-123")
@@ -187,8 +225,8 @@ func TestFilterCallbackWithEndSignal(t *testing.T) {
 
 // Test end signal handling
 func TestEndSignalHandling(t *testing.T) {
-	input := NewMockMiddleware()
-	output := NewMockMiddleware()
+	input := newStubConsumer()
+	output := newStubProducer()
 
 	// Create filter with workersCount = 1 (single worker scenario)
 	filter := NewFilter("worker-1", input, output, "TbyYear", 1, nil, "job-123")
@@ -215,7 +253,8 @@ func TestEndSignalHandling(t *testing.T) {
 	time.Sleep(500 * time.Millisecond)
 
 	// Should have one end signal message in output (since workersCount = 1)
-	messageBytes := output.GetOneMessage()
+	output.waitForAMessage()
+	messageBytes := output.sentMessages[0]
 	var msg ic.Message
 	if err := msg.Unmarshal(messageBytes); err != nil {
 		t.Fatalf("failed to unmarshal output message: %v", err)
@@ -223,5 +262,49 @@ func TestEndSignalHandling(t *testing.T) {
 
 	if msg.Type != ic.MsgEndSignal {
 		t.Fatalf("expected payload to be of type []*ic.EndSignal")
+	}
+}
+
+func TestFiltersDuplicateEndSignal(t *testing.T) {
+	numOfWorkers := 3
+	input := newStubConsumer()
+	output := newStubProducer()
+
+	waitToEnd := make([]chan struct{}, numOfWorkers)
+	for i := 0; i < numOfWorkers; i++ {
+		workerId := fmt.Sprintf("worker-%d", i+1)
+		filter := NewFilter(workerId, input, output, "TbyYear", 1, make(chan string, 1), "job-123")
+		waitToEnd[i] = make(chan struct{}, 1)
+		go func(i int, f *FilterWorker) {
+			f.Start()
+			waitToEnd[i] <- struct{}{}
+		}(i, filter)
+		input.waitForStart()
+	}
+
+	// Send duplicate end signals
+	endSignal, _ := ic.NewEndSignal(nil, 0).Marshal()
+	input.SimulateMessage(endSignal)
+	input.SimulateMessage(endSignal)
+
+	// Wait for workers to finish
+	for i := 0; i < numOfWorkers; i++ {
+		<-waitToEnd[i]
+	}
+
+	endSignalCount := 0
+	for _, msg := range output.sentMessages {
+		var outputMsg ic.Message
+		if err := outputMsg.Unmarshal(msg); err != nil {
+			t.Fatalf("Failed to unmarshal output message: %v", err)
+		}
+		if outputMsg.Type != ic.MsgEndSignal {
+			continue
+		}
+		endSignalCount++
+	}
+
+	if endSignalCount == 0 {
+		t.Fatalf("No end signal messages were sent by the workers")
 	}
 }
